dc341035b298c94f3ed10549452d43bf25a6db8a
marcialvarela <marcial.varela@gmail.com>
2015-02-10 09:05:21 +0100

    20150210_03

--- www/js/index.js
+++ www/js/index.js
@@ -97,7 +97,11 @@ function verCalendario() {
 
     // create a calendar (iOS only for now)
     alert('antes de createCalendar');
-    window.plugins.calendar.createCalendar(calendarName,onSuccess,onError);
+    //window.plugins.calendar.createCalendar(calendarName,onSuccess,onError);
+
+    //function(title, location, notes, startDate, endDate, successCallback, errorCallback) {
+    //window.plugins.createEvent(title, location, notes, startDate, endDate, successCallback, errorCallback);
+    window.plugins.calendar.createEvent(title, location, notes, startDate, endDate, onSuccess, onError);
     alert('después de createCalendar');
 
 }
