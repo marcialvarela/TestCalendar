b5e00a6ecd10682cef939bd3f261a9b8e73e7050
marcialvarela <marcial.varela@gmail.com>
2015-02-11 12:02:34 +0100

    20150211_04

--- www/js/calendar.js
+++ www/js/calendar.js
@@ -1,20 +1,27 @@
 var calendarPlugin = {
     createEvent: function(title, location, notes, startDate, endDate, successCallback, errorCallback) {
         alert('js:calendarPlugin title: ' + title );
-        cordova.exec(
-            successCallback,
-            errorCallback,
-            'calendarPlugin',
-            'addCalendarEntry',
-            [{
-                "title": title,
-                "description": notes,
-                "eventLocation": location,
-                "startTimeMillis": startDate.getTime(),
-                "endTimeMillis": endDate.getTime()
-            }]
-        );
-        alert('js:pasa cordova.exec');
+        try{
+            alert('entra try');
+            cordova.exec(
+                successCallback,
+                errorCallback,
+                'calendarPlugin',
+                'addCalendarEntry',
+                [{
+                    "title": title,
+                    "description": notes,
+                    "eventLocation": location,
+                    "startTimeMillis": startDate.getTime(),
+                    "endTimeMillis": endDate.getTime()
+                }]
+            );
+            alert('js:pasa cordova.exec');
+        }
+        catch (ex) {
+            alert("calendarPlugin.createEvent error: "+ex.message);
+        }
+
      }
 }
 module.exports = calendarPlugin;
